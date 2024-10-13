225f8646090f67d34f6457e8895e94b35dbc8e78
HYH <dudgns3864@gmail.com>
2024-10-14 00:42:11 +0900

    Fix: aws_modify

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running smoothly.");
+});
+
 const startServer = async () => {
   await connectDB();
   app.listen(PORT, () =>
