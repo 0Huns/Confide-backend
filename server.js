44111048c8918cb64c824390c5458ea30a0178f1
HYH <dudgns3864@gmail.com>
2024-10-13 23:34:28 +0900

    Fix: origin-modify

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
-require('dotenv').config();
-const express = require('express')
-const cors = require('cors');
-const { connectDB } = require('./db');
-const { getDB } = require('./db');
-const router = require('./routes');
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { connectDB } = require("./db");
+const { getDB } = require("./db");
+const router = require("./routes");
+const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true 
-}));
+app.use(
+  cors({
+    origin: "https://confide-service.netlify.app/",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 const startServer = async () => {
   await connectDB();
-  app.listen(PORT, () => console.log(`http://localhost:${PORT} 에서 서버 실행 중`));
+  app.listen(PORT, () =>
+    console.log(`http://localhost:${PORT} 에서 서버 실행 중`)
+  );
 };
 
-app.use('/', router);
+app.use("/", router);
 
 startServer();
