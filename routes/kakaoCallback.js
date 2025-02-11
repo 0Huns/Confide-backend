eed7cea8bd7fa6e55e47ee2e7151612dd0d04a4b
HYH <dudgns3864@gmail.com>
2025-02-11 22:51:52 +0900

    Fix: auth logic

--- routes/kakaoCallback.js
+++ routes/kakaoCallback.js
@@ -1,13 +1,16 @@
-require("dotenv").config();
-
 const kakaoCallback = async (req, res) => {
-  //클라이언트에서 인가코드 받기
-  const code = req.query.code;
-  const state = crypto.randomBytes(16).toString("hex");
-  req.session.oauthState = state;
-  res.redirect(
-    `https://confide-service.netlify.app/auth?code=${code}&state=${state}`
-  );
-};
+  if (!req.query.code) {
+    if (!res.headersSent) {
+      return res.status(400).json({ error: "카카오 로그인 코드 없음" });
+    }
+  }
 
-module.exports = kakaoCallback;
+  console.log("카카오에서 받은 code:", req.query.code);
+  console.log("카카오에서 받은 state:", req.query.state);
+
+  if (!res.headersSent) {
+    res.redirect(
+      `https://confide-service.netlify.app/auth?code=${req.query.code}&state=${req.query.state}`
+    );
+  }
+};
